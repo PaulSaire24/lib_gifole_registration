2d4703406acb4a748126587148d84c4591cd2f28
XP62284 <percy.rios.contractor@bbva.com>
2023-03-21 12:17:51 -0500

    PERAR-21798 FEAT: Adiciono internal connection con el aso SMGG20211203

--- RBVDR044IMPL/src/main/java/com/bbva/rbvd/lib/r044/impl/RBVDR044Impl.java
+++ RBVDR044IMPL/src/main/java/com/bbva/rbvd/lib/r044/impl/RBVDR044Impl.java
@@ -1,20 +1,55 @@
 package com.bbva.rbvd.lib.r044.impl;
 
+import com.bbva.rbvd.dto.internalapi.aso.gifole.GifoleInsuranceRequestASO;
+import com.bbva.rbvd.dto.internalapi.util.JsonHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 
-/**
- * The RBVDR044Impl class...
- */
+import java.nio.charset.StandardCharsets;
+
+import static com.bbva.rbvd.dto.internalapi.util.ApiUriKeys.GIFOLE;
+import static com.bbva.rbvd.dto.internalapi.util.Constants.APPLICATION;
+import static com.bbva.rbvd.dto.internalapi.util.Constants.JSON;
+import static com.bbva.rbvd.dto.internalapi.util.Errors.ERROR_CONNECTION_GIFOLE;
+import static org.springframework.http.HttpMethod.POST;
+
 public class RBVDR044Impl extends RBVDR044Abstract {
-
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(RBVDR044Impl.class);
 
-	/**
-	 * The execute method...
-	 */
 	@Override
-	public void execute() {
-		// TODO - Implementation of business logic
+	public Integer executeGifoleRegistration(GifoleInsuranceRequestASO requestBody) {
+		LOGGER.info("***** RBVDR044Impl - executeGifoleRegistration START *****");
+		String jsonString = getRequestJson(requestBody);
+		LOGGER.info("***** RBVDR044Impl - executeGifoleRegistration ***** Request body: {}", jsonString);
+		Integer httpStatus = null;
+		HttpEntity<String> entity = new HttpEntity<>(jsonString, createHttpHeaders());
+		try {
+			ResponseEntity<Void> response = this.internalApiConnector.exchange(GIFOLE.getConsoleKey(), POST, entity, Void.class);
+			httpStatus = response.getStatusCode().value();
+			LOGGER.info("***** RBVDR044Impl - executeGifoleRegistration ***** Http code response: {}", httpStatus);
+		} catch(RestClientException ex) {
+			LOGGER.debug("***** RBVDR044Impl - executeGifoleRegistration ***** Exception: {}", ex.getMessage());
+			this.addAdvice(ERROR_CONNECTION_GIFOLE.getAdviceCode());
+		}
+		LOGGER.info("***** RBVDR044Impl - executeGifoleRegistration END *****");
+		return httpStatus;
 	}
+
+	private String getRequestJson(Object o) {
+		return JsonHelper.getInstance().serialization(o);
+	}
+
+	private HttpHeaders createHttpHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		MediaType mediaType = new MediaType(APPLICATION,JSON, StandardCharsets.UTF_8);
+		headers.setContentType(mediaType);
+		return headers;
+	}
+
 }
--- RBVDR044IMPL/src/test/java/com/bbva/rbvd/lib/r044/RBVDR044Test.java
+++ RBVDR044IMPL/src/test/java/com/bbva/rbvd/lib/r044/RBVDR044Test.java
@@ -3,17 +3,33 @@ package com.bbva.rbvd.lib.r044;
 import com.bbva.elara.configuration.manager.application.ApplicationConfigurationService;
 import com.bbva.elara.domain.transaction.Context;
 import com.bbva.elara.domain.transaction.ThreadContext;
-import javax.annotation.Resource;
-import org.junit.Assert;
+import com.bbva.elara.utility.api.connector.APIConnector;
+import com.bbva.rbvd.dto.internalapi.aso.gifole.GifoleInsuranceRequestASO;
+import com.bbva.rbvd.lib.r044.impl.RBVDR044Impl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
-import org.springframework.aop.framework.Advised;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestClientException;
+
+import javax.annotation.Resource;
+
+import static com.bbva.rbvd.dto.internalapi.util.Errors.ERROR_CONNECTION_GIFOLE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -22,37 +38,53 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 		"classpath:/META-INF/spring/RBVDR044-arc.xml",
 		"classpath:/META-INF/spring/RBVDR044-arc-test.xml" })
 public class RBVDR044Test {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RBVDR044Test.class);
 
-	@Spy
 	private Context context;
 
-	@Resource(name = "rbvdR044")
-	private RBVDR044 rbvdR044;
+	private RBVDR044Impl rbvdr044 = new RBVDR044Impl();
 
 	@Resource(name = "applicationConfigurationService")
 	private ApplicationConfigurationService applicationConfigurationService;
 
+	private APIConnector internalApiConnector;
+
+	private static final String MESSAGE_EXCEPTION = "CONNECTION ERROR";
+
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 		MockitoAnnotations.initMocks(this);
 		context = new Context();
 		ThreadContext.set(context);
-		getObjectIntrospection();
+		
+		internalApiConnector = Mockito.mock(APIConnector.class);
+		rbvdr044.setInternalApiConnector(internalApiConnector);
 	}
-	
-	private Object getObjectIntrospection() throws Exception{
-		Object result = this.rbvdR044;
-		if(this.rbvdR044 instanceof Advised){
-			Advised advised = (Advised) this.rbvdR044;
-			result = advised.getTargetSource().getTarget();
-		}
-		return result;
-	}
-	
+
 	@Test
-	public void executeTest(){
-		rbvdR044.execute();
-		Assert.assertEquals(0, context.getAdviceList().size());
+	public void executeGifoleRegistrationServiceOK() {
+		LOGGER.info("RBVDR044Test - Executing executeGifoleServiceOK...");
+
+		when(this.internalApiConnector.exchange(anyString(), any(HttpMethod.class), anyObject(), (Class<Void>) any()))
+				.thenReturn(new ResponseEntity<>(HttpStatus.CREATED));
+
+		Integer validation = rbvdr044.executeGifoleRegistration(new GifoleInsuranceRequestASO());
+
+		assertNotNull(validation);
+		assertEquals(new Integer(201), validation);
+	}
+
+	@Test
+	public void executeGifoleRegistrationServiceWithRestClientException() {
+		LOGGER.info("RBVDR044Test - Executing executeGifoleServiceWithRestClientException...");
+
+		when(this.internalApiConnector.exchange(anyString(), any(HttpMethod.class), anyObject(), (Class<Void>) any()))
+				.thenThrow(new RestClientException(MESSAGE_EXCEPTION));
+
+		Integer validation = rbvdr044.executeGifoleRegistration(new GifoleInsuranceRequestASO());
+
+		assertNull(validation);
+		assertEquals(ERROR_CONNECTION_GIFOLE.getAdviceCode(), this.rbvdr044.getAdviceList().get(0).getCode());
 	}
 	
 }
